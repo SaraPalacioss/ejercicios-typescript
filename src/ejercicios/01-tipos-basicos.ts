0a3d4232483b3a11fa9cf5989ff46a2e77358c90
sarastackhouse <sarapalacios7@gmail.com>
2021-02-07 19:47:12 +0100

    variables

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,16 @@
+
+
+console.log('Hola Mundo!!!!');
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let person: string = "sara";
+let number: number | string = 95;
+let vivo : boolean = false;
+
+nombre = 'Fernando';
+hp = 'lola';
+
+console.log(nombre, hp, alive);
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+
+
+console.log('Hola Mundo!!!!');
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = "sara";
+let hp: number | string = 95;
+let alive : boolean = false;
+
+nombre = 'Fernando';
+hp = 'lola';
+
+console.log(nombre, hp, alive);
